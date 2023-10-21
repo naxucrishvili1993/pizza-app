e4b8fa21e8b4e212c1a96b054f7bc8e195b88bdd
Giorgi <giorginaxucrishvili70@gmail.com>
2023-10-21 23:50:29 +0400

    Deploy my Pizza App to GitHub

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+import Cart from "./features/cart/Cart";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import CreateOrder, {
+  action as createOrderAction,
+} from "./features/order/CreateOrder";
+import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
+
+import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
+import Home from "./ui/Home";
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    errorElement: <Error />,
+
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/menu",
+        element: <Menu />,
+        errorElement: <Error />,
+        loader: menuLoader,
+      },
+      { path: "/cart", element: <Cart /> },
+      {
+        path: "/order/new",
+        element: <CreateOrder />,
+        action: createOrderAction,
+      },
+      {
+        path: "/order/:orderID",
+        element: <Order />,
+        loader: orderLoader,
+        errorElement: <Error />,
+        action: updateOrderAction,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
